aa3eb6eb67bd25c4054d4a9a0a4d595c35c1fc83
Peter Nguyen <peter.p.t.nguyen@gmail.com>
2023-04-03 21:57:10 +1000

    Initial commit

---
+++ postcodeapi/src/main/java/peter/postcodeapi/PostcodeapiApplication.java
@@ -0,0 +1,13 @@
+package peter.postcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcodeapiApplication.class, args);
+	}
+
+}
